1c67eb44f30f47d13cdca0f984add31f3a8dbfb2
LayanAlomari <s202351790@kfupm.edu.sa>
2026-09-19 22:25:20 +0300

    add dark mode functionality with theme toggle button in index.html and script.js, and update styles.css for dark mode support

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ if (hour < 12) {
 } else {
   greeting.textContent = "Good evening!";
 }
+
+// Dark/light mode button
+const themeButton = document.getElementById("theme-toggle");
+
+themeButton.addEventListener("click", function () {
+  // Add the class if it's missing, remove it if it's there
+  document.body.classList.toggle("dark-mode");
+
+  // Change the button text to match
+  if (document.body.classList.contains("dark-mode")) {
+    themeButton.textContent = "Light Mode";
+  } else {
+    themeButton.textContent = "Dark Mode";
+  }
+});
